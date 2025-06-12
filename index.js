a8bd84daec3c945a62d466fc8d52959f5e7d21ca
Oliver Lomanat <lomanatoliver@gmail.com>
2025-06-12 08:21:47 +0300

    Adds variables to index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = `Welcome, + ${firstInitial}!`
